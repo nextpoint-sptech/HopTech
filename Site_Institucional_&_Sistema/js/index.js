1e1f95564d13b06b04bca289de52e8b38dc7cba5
Kauanny Tenorio <kakau24112004@hotmail.com>
2023-04-12 22:29:47 -0300

    Adicionando função de rolagem no header da página

---
+++ Site_Institucional_&_Sistema/js/index.js
@@ -0,0 +1,19 @@
+const header = document.getElementById("header")
+        , navbar = document.getElementById("navbar");
+
+function rolagem(){
+        header.classList.toggle("ativa" , scrollY > 0);
+}
+
+window.addEventListener('scroll', rolagem);
+
+function enviarMensagem(){
+        var nomeEmpresa = ipt_nome_empresa.value;
+        var telefoneEmpresa = ipt_telefone_empresa.value;
+        var emailEmpresa = ipt_email_empresa.value;
+        var mensagem = ipt_mensagem_empresa.value;
+
+        if(nomeEmpresa != "" && telefoneEmpresa != "" && emailEmpresa != "" && mensagem != ""){
+                alert("Mensagem enviada com sucesso! Logo entraremos em contato :)");
+        }
+}
