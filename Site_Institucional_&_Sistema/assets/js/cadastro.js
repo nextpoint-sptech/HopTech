23603e21e1f0ce22524bd91197d520d222983a8e
Kauanny Tenorio <kakau24112004@hotmail.com>
2023-04-17 11:49:56 -0300

    Adicionando arquivos para a construção da tela de cadastro

---
+++ Site_Institucional_&_Sistema/assets/js/cadastro.js
@@ -0,0 +1,11 @@
+function sair(){
+    window.parent.location.href = "../../login.html";
+}
+
+function irParaDashboard(){
+    window.parent.location.href = "./dashboardHoptech.html";
+}
+
+function irParaCadastro(){
+    window.parent.location.href = "./cadastro.html";
+}
