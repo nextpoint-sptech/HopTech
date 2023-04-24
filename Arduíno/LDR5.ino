2a838ce1a6b3a2491d747835893e3d0b8969eefb
lucasgianine <lucas.esilva@sptech.school>
2023-04-24 19:09:30 -0300

    Código Arduíno do sensor LDR5 em C++

--- Arduíno/LDR5.ino
+++ Arduíno/LDR5.ino
@@ -1,16 +1,29 @@
 /**
 * Colocando a porta de entrada e qual sensor vamos usar
 */
-
 int ldr_pin = A0, leitura_ldr = 0;
 
+/**
+* Contador:
+*/
+int contador = 1;
+
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600);
 }
 
 void loop() {
-leitura_ldr = analogRead(ldr_pin);
-Serial.print(leitura_ldr);
-Serial.print(";");
-delay(2000);
+  leitura_ldr = analogRead(ldr_pin);
+  
+  Serial.print(contador);
+  Serial.print('.');
+  
+  Serial.print(leitura_ldr);
+  Serial.println(";");
+  
+  delay(2000);
+  
+  contador++;
 }
+
+
