efa11669f212cb6fd22462f0896efaaa60c9dd1b
Kauanny Tenorio <kakau24112004@hotmail.com>
2023-05-06 00:54:05 -0300

    Criando página de suporte

---
+++ Site_Institucional_&_Sistema/assets/js/suporte.js
@@ -0,0 +1,42 @@
+function sair(){
+    window.parent.location.href = "../login.html";
+}
+
+function goDashboard(){
+    window.parent.location.href = "./dashboard.html";
+}
+
+function goUsuarios(){
+    window.parent.location.href = "../erro-pages/erro404-hoptech.html";
+}
+
+function goHistorico(){
+    window.parent.location.href = "../erro-pages/erro404-hoptech.html";
+}
+
+function irParaSuporte(){
+    window.parent.location.href = "./suporte.html";
+}
+
+function realizarPedidoSuporte(){
+    var tituloPedido = ipt_titulo_assunto_suporte.value;
+    var descricaoSuporte = ipt_descricao_suporte.value;
+
+    if(tituloPedido != "" && descricaoSuporte != ""){
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Pedido de suporte realizado com sucesso!',
+            text: 'Aguarde que logo entraremos em contato.',
+            showConfirmButton: true
+        })
+    }else{
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Campo obrigatório vazio.',
+            text: 'É necessário preencher todos os campos para relizar o pedido de suporte.',
+            showConfirmButton: true,
+        });
+    }
+}
