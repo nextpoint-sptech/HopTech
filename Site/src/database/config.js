66ede724def99e62a68b4cb7872110f7681a2e2f
nayrabelarmino <nayra.belarmino@sptech.school>
2023-06-02 17:21:12 -0300

    Ajuste CSS na tela de permissoes

--- Site/src/database/config.js
+++ Site/src/database/config.js
@@ -21,8 +21,8 @@ var sql = require('mssql');
 var mySqlConfig = {
     host: "localhost",
     database: "hop_tech",
-    user: "admin_hop_tech",
-    password: "1234",
+    user: "root",
+    password: "Na121709_",
 };
 
 function executar(instrucao) {
